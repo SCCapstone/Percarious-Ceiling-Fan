a93af8efba4a43536e539723d7335aa3b9f5bfcd
rdsoard <srebekahd@gmail.com>
2021-11-10 21:14:10 -0500

    connecting client and server, updating server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const cors = require("cors");
+const connection = require('./db');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
