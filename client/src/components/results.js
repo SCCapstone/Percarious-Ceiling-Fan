73d667fc750607f5b1968ae3864d00396bfa5e2e
caeblue <caeblue11@gmail.com>
2021-11-21 20:45:00 -0500

    work on results page

---
+++ client/src/components/results.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import axios from "axios"; //for SQL command stuff
+
+class Results extends React.Component{
+    state = {
+        connection: ""
+    }
+
+    componentDidMount() {
+		console.log('this runs');
+		this.getConnectionTest();
+	}
+
+    getConnectionTest = () => {
+		axios.get('http://localhost:3001/')
+		.then((response) => response.data)
+		.then(response => {this.setState({connection: response})})
+	};
+
+    render(){
+        return (
+			<div>
+				<div class="content-wrapper">
+					<div class="nav-bar-wrapper">
+						<div class="menu-icon"></div>
+						<p class="header">Results Page</p>
+					</div>
+					<div class="main-body">
+						<h1>Welcome to The Precarious Search System</h1>
+					</div>
+					
+					
+					<div class="bottom-nav-bar">
+						<div class="left-side">
+							<p class="goal-header">Our goal:</p>
+							<p>blah blah blah info info info</p>
+						</div>
+						<div class="right-side">
+							<p class="contact-header">Contact Us:</p>
+							<p>#2709 : testEmail@gmail.com</p>
+							<p>#</p>
+						</div>
+					</div>
+    			</div>
+			</div>
+		)
+    }
+}
+export default Results;
