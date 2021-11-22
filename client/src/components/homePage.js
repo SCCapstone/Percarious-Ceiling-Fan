d12f1d3458a1c9e995aff4d497c907e4e8d11673
rdsoard <srebekahd@gmail.com>
2021-11-21 20:12:52 -0500

    moved hompage html to proper place, added search bar with incomplete functionality

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -3,26 +3,55 @@ import axios from "axios"; //for SQL command stuff
 
 class HomePage extends React.Component{
 	state = {
-		connection: ""
+		connection: "",
+		search: ""
 	}
 
 	componentDidMount() {
 		console.log('this runs');
-		this.getConnectionTest();
 	}
 
-	getConnectionTest = () => {
-		axios.get('http://localhost:3001/')
+	onSubmitClick = () => {
+		axios.post('http://localhost:3001/basicSearch',{ //TO DO
+			search: this.state.search
+		})
+		this.getBasicSearchResult();
+	}
+
+	//get the results and round them to results page here
+	getBasicSearchResult = () => {
+		axios.get('http://localhost:3001/basicSearch')
 		.then((response) => response.data)
-		.then(response => {this.setState({connection: response})})
+		.then(response => {this.setState({connection: response})}) //placeholder, TODO
 };
+
 	render() {
 		return (
 			<div>
-			<div className="ui input">
-			<input value={this.state.example} onChange={e => this.setState({ example: e.target.value})} placeholder="Search"/>
+				<div class="content-wrapper">
+      			<div class="nav-bar-wrapper">
+        		<div class="menu-icon"></div>
+        <p class="header">Home</p>
+      </div>
+      <div class="main-body">
+        <h1>Welcome to The Precarious Search System</h1>
+      </div>
+	  <div className="ui input">
+			<input value={this.state.search} onChange={e => this.setState({ search: e.target.value})} placeholder="Search"/> 
 			</div>
-			<button className= "ui large primary button" onClick={() => this.onSubmitClick()}>Add</button>
+			<button className= "ui large primary button" onClick={() => this.onSubmitClick()}>Search</button>
+      <div class="bottom-nav-bar">
+        <div class="left-side">
+          <p class="goal-header">Our goal:</p>
+          <p>blah blah blah info info info</p>
+        </div>
+        <div class="right-side">
+          <p class="contact-header">Contact Us:</p>
+          <p>#2709 : testEmail@gmail.com</p>
+          <p>#</p>
+        </div>
+      </div>
+    </div>
 			</div>
 		)
 	}
