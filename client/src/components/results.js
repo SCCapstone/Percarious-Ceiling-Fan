581c0fbdd8692a7061697951f4e7de35b914a53a
Evan Bryer <evanbryer@gmail.com>
2021-11-26 18:41:31 -0500

    Added function to get decade counts from sql response

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import axios from "axios"; //for SQL command stuff
 
+function getDecades(data){
+	decades = {};
+	for(let i = 0; i < data.length; i++){
+		year = data[i].control_string.substring(7,11);
+		dec = Math.trunc(year/10)*10;
+		if(!decades[dec]) decades[dec] = 0;
+		decades[dec]++;
+	}
+	return decades;
+}
+
 class Results extends React.Component{
     state = {
         connection: ""
@@ -28,8 +39,8 @@ class Results extends React.Component{
 					<div class="main-body">
 						<h1>Welcome to The Precarious Search System</h1>
 					</div>
-					
-					
+
+
 					<div class="bottom-nav-bar">
 						<div class="left-side">
 							<p class="goal-header">Our goal:</p>
