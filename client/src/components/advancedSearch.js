5553fc3ba69e3e234874eff409d18eca53ceb981
caeblue <caeblue11@gmail.com>
2021-11-30 15:27:48 -0500

    advanced search

--- client/src/components/advancedSearch.js
+++ client/src/components/advancedSearch.js
@@ -33,43 +33,45 @@ class AdvancedSearch extends React.Component{
 		<div>
 		<title>Advanced Search</title>
 		<body>
-			<div id="search list">
+			<div class = "advanced-search-body">
+			<div id="search-list">
 				<div attribute="searchTerms">
 					
 				</div>
-				<div attribute="searchTerms">
+				<div class = "any-words" attribute="searchTerms">
 					<p attribute="searchPrompt">Any of these words:</p>
 					<input attribute = "searchInput" id= "anyWords" value={this.state.anyWords} onChange={e => this.setState({ anyWords: e.target.value})}></input>
 				</div>
-				<div attribute="searchTerms">
+				<div attribute="searchTerms" class = "exact-words">
 					<p attribute="searchPrompt">Exact words or phrases</p>
 					<input attribute = "searchInput" id="exactPhrase" value={this.state.exactPhrase} onChange={e => this.setState({ exactPhrase: e.target.value})}></input>
 				</div>
-				<div attribute="searchTerms">
+				<div attribute="searchTerms" class ="exclude-words">
 					<p attribute="searchPrompt">None of these words:</p>
 					<input attribute = "searchInput" id="exclude" value={this.state.exclude} onChange={e => this.setState({ exclude: e.target.value})}></input>
 				</div>
-				<div attribute="searchTerms">
+				<div attribute="searchTerms" class = "query-tag">
 					<p attribute="searchPrompt">Tags to query:</p>
 					<input attribute = "searchInput" id="tags" value={this.state.tags} onChange={e => this.setState({ tags: e.target.value})}></input>
 				</div>
-				<div attribute="searchTerms">
+				<div attribute="searchTerms" class ="year-range">
 					<p attribute="searchPrompt">Year range</p>
 					<input attribute = "searchInput" id="startYear" value={this.state.startYear} onChange={e => this.setState({ startYear: e.target.value})}></input>
 					<p attribute = "searchPrompt">to</p>
 					<input attribute = "searchInput" id="endYear" value={this.state.endYear} onChange={e => this.setState({ endYear: e.target.value})}></input>
 				</div>
-				<div attribute="searchTerms">
+				<div attribute="searchTerms" class = "languages">
 					<p attribute="searchPrompt">Languages:</p>
 					<input attribute = "searchInput" id="languages" value={this.state.languages} onChange={e => this.setState({ languages: e.target.value})}></input>
 				</div>
-				<div attribute="searchTerms">
+				<div attribute="searchTerms" class="region">
 					<p attribute="searchPrompt">Region:</p>
 					<input attribute = "searchInput" id="regions" value={this.state.regions} onChange={e => this.setState({ regions: e.target.value})}></input>
 				</div>
 			</div>
 			<div id="search button">
 				<button>Search</button>
+			</div>
 			</div>
 		</body>
 			</div>
