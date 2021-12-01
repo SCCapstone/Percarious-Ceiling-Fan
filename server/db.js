02758b8a7f8232eed3bb9ce20b270856de9e9272
Tyron Schultz <47118411+Sharpscore247@users.noreply.github.com>
2021-12-01 18:56:27 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'Ewhzfig4.',
+	password: 'Precarious!.',
 	database: 'bookdata'
 })
 
