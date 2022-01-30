31ba0f570f23bc829eea75846b5a9e05ef9d4ef6
Tyron Schultz <tyronsonfire@gmail.com>
2022-01-29 21:51:47 -0500

    testing different syntax for unit testing

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'Percarious!',
-	database: 'bookdata'
+	password: 'Precarious!',
+	database: 'bookdatacut'
 })
 
 module.exports = connection;
