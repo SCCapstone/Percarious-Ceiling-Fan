f58d64193190665150c499bca3d5065974c082a5
rdsoard <srebekahd@gmail.com>
2022-01-29 21:42:43 -0500

    editted db.test.js

--- server/tests/db.test.js
+++ server/tests/db.test.js
@@ -1,7 +1,7 @@
 var test = require('unit.js');
 const connection = require("../db");
 
-test.value(connection.host).match('localhost')
-	.value(connection.user).match('root')
-	.value(connection.password).match('Percarious!')
-	.value(connection.password).match('bookdata');
+test.value(connection.host).is('localhost')
+	.value(connection.user).is('root')
+	.value(connection.password).is('Percarious!')
+	.value(connection.database).is('bookdata');
