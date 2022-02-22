e3d83aefa1e7a394ae442fd9dc0dc601d1ebb8ca
rdsoard <srebekahd@gmail.com>
2022-02-22 13:14:57 -0500

    initializing firebase in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,20 @@ import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import HomePage from "./components/homePage";
 import AdvancedSearch from './components/advancedSearch';
 import Results from './components/results';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: "AIzaSyApxySs35Gvjx5ImJpPsi5YQgLGUIGuA9Q",
+	authDomain: "oclc-advanced-search-system.firebaseapp.com",
+	projectId: "oclc-advanced-search-system",
+	storageBucket: "oclc-advanced-search-system.appspot.com",
+	messagingSenderId: "683111933545",
+	appId: "1:683111933545:web:48f08c8e006652439224f8",
+	measurementId: "G-NPEH2715VT"
+  };
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
 
 function App() {
   return (
