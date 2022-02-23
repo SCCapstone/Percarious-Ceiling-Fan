bc20f456ccf89b87ace39c179c44dce7dc9e35d1
rdsoard <srebekahd@gmail.com>
2022-02-23 15:57:22 -0500

    changing sign up page and fixing error, npm install react bootstrap

--- client/src/components/signIn.js
+++ client/src/components/signIn.js
@@ -1,13 +1,14 @@
 import React, { useRef, useState } from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
 import { useAuth } from "../contexts/AuthContext"
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from "react-router-dom"
 
-export default function SignIn() {
+export default function Login() {
   const usernameRef = useRef()
   const passwordRef = useRef()
-  const { signin } = useAuth()
-//   const [error, setError] = useState("")
-//   const [loading, setLoading] = useState(false)
+  const { login } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
   const history = useHistory()
 
   async function handleSubmit(e) {
@@ -16,21 +17,39 @@ export default function SignIn() {
     try {
       setError("")
       setLoading(true)
-      await signin(usernameRef.current.value, passwordRef.current.value)
+      await login(usernameRef.current.value, passwordRef.current.value)
       history.push("/")
     } catch {
-      setError("Failed to sign in")
+      setError("Failed to log in")
     }
 
     setLoading(false)
   }
 
   return (
-	  <div>
-		  <h1>Username:</h1>
-		  <input id="username" style = {inputStylingBasic} value={username} onChange={e => ref={usernameRef}}/>
-		  <h1>Password:</h1>
-		  <input id="passworn" style = {inputStylingBasic} value={password} onChange={e => ref={passwordRef}}/>
-	  </div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Log In</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
+            <Form.Group id="username">
+              <Form.Label>username</Form.Label>
+              <Form.Control type="username" ref={usernameRef} required />
+            </Form.Group>
+            <Form.Group id="password">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" ref={passwordRef} required />
+            </Form.Group>
+            <Button disabled={loading} className="w-100" type="submit">
+              Log In
+            </Button>
+          </Form>
+          <div className="w-100 text-center mt-3">
+            <Link to="/forgot-password">Forgot Password?</Link>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
