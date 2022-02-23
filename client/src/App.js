f06290a646a0d44378f1b1a24ee1ad50e51631d9
rdsoard <srebekahd@gmail.com>
2022-02-23 14:14:50 -0500

    signin page linked on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import HomePage from "./components/homePage";
 import AdvancedSearch from './components/advancedSearch';
+import SignIn from './components/signIn';
 import Results from './components/results';
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from 'firebase/firestore';
@@ -26,11 +27,13 @@ function App() {
 				<div class="content-wrapper">
 					<div class="nav-bar-wrapper">
 					<nav class = "home-link"><Link to="/" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '25px', textTransform: 'uppercase'}}>Home</Link></nav>
+					<nav class = "home-link"><Link to="/signIn" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '25px', textTransform: 'uppercase'}}>Sign In</Link></nav>
 						<div class="menu-icon"></div>
 						</div>
 						<Switch>
 							<Route path="/advancedSearch"> <AdvancedSearch /> </Route>
 							<Route path="/results"> <Results /> </Route>
+							<Route path="/signIn"> <SignIn /> </Route>
 							<Route path="/"> <HomePage /> </Route>
 						</Switch>
 						<div class = "bottom-nav-bar">
--- client/src/components/signIn.js
+++ client/src/components/signIn.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class SignIn extends React.Component{
 	componentDidMount() {
 		console.log('sign in page');	
 	};
 	render() {
-		
+		return(
+			<p>Hi</p>
+		)
 	}
-};
+}
 export default SignIn;
