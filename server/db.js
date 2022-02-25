57d07f9de526a7d87160a92d3ba2743476dd74ca
caeblue <caeblue11@gmail.com>
2022-02-25 13:31:16 -0500

    whoops

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2')
 const connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: 'Jrcm8616!!',
-	database: 'bookdatacut'
+	password: 'Precarious!',
+	database: 'bookdata'
 })
 
 module.exports = connection;
