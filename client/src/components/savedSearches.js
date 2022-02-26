a6e65fffe68bcb1aae474501f745301055c45243
Sharpscore247 <tyronsonfire@gmail.com>
2022-02-25 20:13:28 -0500

    log out button added

--- client/src/components/savedSearches.js
+++ client/src/components/savedSearches.js
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from "react";
 import FirebaseService from "../services/firebase.service";
+import { useAuthContext } from "../contexts/AuthContext";
+import {Button} from "react-bootstrap"
+import { useHistory } from "react-router-dom"
 
 const SavedSearches = () => {
+  const { logOut } = useAuthContext();
+  const history = useHistory()
   const [searches, setSearches] = useState([]);
   useEffect(() => {
     getSearches();
   }, []);
 
+  const logoutHandler = async () => {
+    try {
+      await logOut();
+      history.push("/")
+     // console.log(useAuthContext());
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
   const getSearches = async () => {
-    const data = await FirebaseService.getSearches();
-    console.log(data.docs);
-    setSearches(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    //const data = await FirebaseService.getSearches();
+    //console.log(data.docs);
+    //setSearches(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
   };
 
   return (
@@ -21,6 +36,10 @@ const SavedSearches = () => {
 		<p></p>
 		</div>
 		))}
+
+    <div className="button">
+      <Button variant="primary" onClick={logoutHandler}>Log Out</Button>
+    </div>
 	</div>
   );
 };
