7e3a561ac0f64676bbce661cca8486d46620ccd3
Tyron Schultz <tyronsonfire@gmail.com>
2022-04-03 19:13:12 -0400

    added db file back in

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'Precarious!',
+	database: 'bookdata'
+})
+
+module.exports = connection;
+ 
