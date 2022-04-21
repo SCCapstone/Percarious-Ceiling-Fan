3dae093c531a3068910d9cbcf6a99fa6d3c11864
rdsoard <srebekahd@gmail.com>
2022-04-21 12:28:02 -0400

    moved logout button to nav bar instead of on bottom of saved searches page, styling needs to be fixed

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,14 +1,25 @@
 import React from "react"
-import {Link} from 'react-router-dom'
+import {Link, useHistory} from 'react-router-dom'
 import { useAuthContext } from "../contexts/AuthContext"
+import { Button} from 'react-bootstrap';
 
 const ProtectedRoute = () => {
+	const { logOut } = useAuthContext();
    const { user } = useAuthContext();
+   const history = useHistory()
+   const logoutHandler = async () => {
+    try {
+      await logOut();
+      history.push("/")
+    } catch (err) {
+      console.log(err)
+    }
+  };
    if(!user) {
         return <nav className= "home-link"><Link to="/signIn" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '35px', textTransform: 'uppercase'}}>Sign In</Link></nav>
     }
 	else {
-		return <nav className= "home-link"><Link to="/savedSearches" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '35px', textTransform: 'uppercase'}}>Searches</Link></nav>
+		return <nav className= "home-link"><Link to="/savedSearches" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '35px', textTransform: 'uppercase'}}>Searches</Link><div className="button" style= {{paddingTop: '10px'}}><Button style= {{color:'white', backgroundColor:'#6675b0', padding: '10px', borderRadius: '5px'}} variant="primary" onClick={logoutHandler}>Log Out</Button></div></nav> 
 	}
 };
 
--- client/src/components/savedSearches.js
+++ client/src/components/savedSearches.js
@@ -2,25 +2,14 @@ import React, { useEffect, useState } from "react";
 import FirebaseService from "../services/firebase.service";
 import { useAuthContext } from "../contexts/AuthContext";
 import {Button} from "react-bootstrap"
-import { Link, useHistory } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const SavedSearches = () => {
-  const { logOut } = useAuthContext();
   const { user } = useAuthContext();
-  const history = useHistory()
   const [searches, setSearches] = useState([]);
   useEffect(() => {
     getSearches();
   }, []);
-
-  const logoutHandler = async () => {
-    try {
-      await logOut();
-      history.push("/")
-    } catch (err) {
-      console.log(err)
-    }
-  };
 
   const getSearches = async () => {
     const data = await FirebaseService.getSearches(user.uid);
@@ -57,9 +46,6 @@ const SavedSearches = () => {
           </div>
     ))}
     </div>
-		<div className="button" style= {{paddingTop: '10px'}}>
-			<Button style= {{color:'white', backgroundColor:'#6675b0', padding: '10px', borderRadius: '5px'}} variant="primary" onClick={logoutHandler}>Log Out</Button>
-		</div>
     </div>
 	</div>
   );
