8edfb87a2062a07c404e7287cafb4d94f9c3e9ea
Tyron Schultz <tyronsonfire@gmail.com>
2022-04-21 13:10:34 -0400

    button styling for sign out

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -19,7 +19,7 @@ const ProtectedRoute = () => {
         return <nav className= "home-link"><Link to="/signIn" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '35px', textTransform: 'uppercase'}}>Sign In</Link></nav>
     }
 	else {
-		return <nav className= "home-link"><Link to="/savedSearches" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '35px', textTransform: 'uppercase'}}>Searches</Link><div className="button" style= {{paddingTop: '10px'}}><Button style= {{color:'white', backgroundColor:'#6675b0', padding: '10px', borderRadius: '5px'}} variant="primary" onClick={logoutHandler}>Log Out</Button></div></nav> 
+		return <nav className= "home-link"><Link to="/savedSearches" style = {{textDecoration: 'none', color: 'white', float: 'left', marginTop: '15px', marginLeft: '35px', textTransform: 'uppercase'}}>Searches</Link><div className="button" style= {{paddingTop: '10px'}}><Button style= {{color:'white', backgroundColor:'#3A2F7D', padding: '10px', borderRadius: '5px', float:"right", marginRight:"5px"}} variant="primary" onClick={logoutHandler}>LOG OUT</Button></div></nav> 
 	}
 };
 
